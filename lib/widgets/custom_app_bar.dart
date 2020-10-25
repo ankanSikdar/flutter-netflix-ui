e0572e1420456f3453e2d8651fdcca4dc3b3b3b3
Ankan Sikdar <ankansikdar@gmail.com>
2020-10-25 21:55:21 +0530

    Added fade effect to app bar

--- lib/widgets/custom_app_bar.dart
+++ lib/widgets/custom_app_bar.dart
@@ -2,10 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter_netflix_responsive_ui/assets.dart';
 
 class CustomAppBar extends StatelessWidget {
+  final double scrollOffset;
+
+  const CustomAppBar({
+    Key key,
+    this.scrollOffset = 0.0,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.redAccent,
+      color:
+          Colors.red.withOpacity((scrollOffset / 350).clamp(0, 1).toDouble()),
       padding: EdgeInsets.symmetric(vertical: 10, horizontal: 24),
       child: SafeArea(
         child: Row(
