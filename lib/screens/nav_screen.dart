32c181bf4c735cd33d44d6d278fd1d513442f000
Ankan Sikdar <ankansikdar@gmail.com>
2020-10-26 21:47:35 +0530

    Added bottom nav bar

---
+++ lib/screens/nav_screen.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_netflix_responsive_ui/screens/screens.dart';
+
+class NavigationScreen extends StatefulWidget {
+  @override
+  _NavigationScreenState createState() => _NavigationScreenState();
+}
+
+class _NavigationScreenState extends State<NavigationScreen> {
+  final List<Widget> _screens = [
+    HomeScreen(),
+    Scaffold(),
+    Scaffold(),
+    Scaffold(),
+    Scaffold(),
+  ];
+
+  final Map<String, IconData> _icons = {
+    'Home': Icons.home,
+    'Search': Icons.search,
+    'Coming Soon': Icons.queue_play_next,
+    'Downloads': Icons.file_download,
+    'More': Icons.menu,
+  };
+
+  int _currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (value) {
+          setState(() {
+            _currentIndex = value;
+          });
+        },
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _currentIndex,
+        backgroundColor: Colors.black,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.grey,
+        selectedFontSize: 11.0,
+        unselectedFontSize: 11.0,
+        items: _icons
+            .map(
+              (title, icon) => MapEntry(
+                title,
+                BottomNavigationBarItem(
+                  icon: Icon(icon),
+                  label: title,
+                ),
+              ),
+            )
+            .values
+            .toList(),
+      ),
+    );
+  }
+}
